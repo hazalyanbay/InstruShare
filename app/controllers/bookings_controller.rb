2c5814a2c76110aa7a30e9a148bfb73a26955277
Hazal Yanbay buse.xezal@gmail.com <buse.xezal@gmail.com>
2020-10-20 17:34:24 +0300

    bookings controller and actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,31 @@
 class BookingsController < ApplicationController
+  def new
+    @instrument = Instrument.find(params[:instrument_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @instrument = Instrument.find(params[:instrument_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.instrument = @instrument
+
+    respond_to do |format|
+      if @booking.save
+        format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :total_price)
+  end
 end
