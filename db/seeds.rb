0a408bb9570caeaab99de564cadd6576528f3f0a
Semyon Kazantsev <semyon.kazantsev@gmail.com>
2020-10-20 18:46:43 +0300

    bd started and instrument controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Instrument.destroy_all
+Booking.destroy_all
+
+sam = User.create(name: 'Sam', email: 'parnik74@yandex.ru', password: '123456')
+hazal = User.create(name: 'Hazal', email: 'hazal@gamil.com', password: '555555')
+guitar = Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+
+Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+Instrument.create(style: 'guitar', rate: 10, location: 'Istanbul', description: 'Wow guitar', user: sam )
+puts Instrument.count
+Booking.create(user: hazal, instrument: guitar )
+#Review.create(content: "Great!", booking_id:)
