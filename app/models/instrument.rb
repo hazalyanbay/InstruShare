8cd1da95ec7f3186cb26e7d138c904e6af9fdc60
Semyon Kazantsev <semyon.kazantsev@gmail.com>
2020-10-21 14:06:52 +0300

    Instrument controller has INDEX SHOW NEW CREATE and Update + validations for model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,3 +1,7 @@
 class Instrument < ApplicationRecord
+  validates :style, presence: true
+  validates :rate, presence: true
+  validates :location, presence: true
+  validates :description, length: { minimum: 5, maximum: 700, too_long: "700 characters is the maximum allowed" }
   belongs_to :user
 end
