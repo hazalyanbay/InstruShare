f4d877402e9b77daf2f5ffa813292d105fc08419
Hazal Yanbay buse.xezal@gmail.com <buse.xezal@gmail.com>
2020-10-22 14:27:17 +0300

    desigs inturments reviews and some buttons

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,12 +2,14 @@ class Instrument < ApplicationRecord
   validates :style, presence: true
   validates :rate, presence: true
   validates :location, presence: true
+  has_many :bookings
+  has_many :reviews, through: :bookings
   validates :description, length: { minimum: 5, maximum: 700 }
   belongs_to :user
   has_many_attached :photos
 
-  
-  def self.styles 
+
+  def self.styles
     ["acoustic guitar", "electric guitar", "drums", "ukulele", "flute"]
   end
 end
